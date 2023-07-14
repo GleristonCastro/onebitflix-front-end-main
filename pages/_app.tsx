555dd0454568be63a8f31095a8272b27d83d28a2
Gleriston <xpanium@gmail.com>
2023-07-14 20:48:57 -0300

    Refactoring for homeNoAuth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import type { AppProps } from 'next/app';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
