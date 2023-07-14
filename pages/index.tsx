acee296b313d7ba0f926e70df8752f70f4c3e134
Gleriston <xpanium@gmail.com>
2023-07-14 15:30:09 -0300

    Configuração inicial do projeto

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import style from '../styles/HomeNoAtuth.module.scss'
+
+const HomeNoAuth = () => {
+  return (
+    <>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <main></main>
+    </>
+  );
+};
+
+export default HomeNoAuth;
