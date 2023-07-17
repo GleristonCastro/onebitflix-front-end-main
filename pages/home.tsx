1c9db0b8afd98cb219420f031cdf736fa1a8143b
Gleriston <xpanium@gmail.com>
2023-07-17 13:05:54 -0300

    Include Newest Category

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,4 +1,5 @@
 import FeaturedSection from '@/src/components/common/homeAuth/featuredSection';
+import NewestCategory from '@/src/components/common/homeAuth/newestCategory';
 import Head from 'next/head';
 
 const HomeAuth = function () {
@@ -10,6 +11,7 @@ const HomeAuth = function () {
       </Head>
       <main>
         <FeaturedSection />
+        <NewestCategory />
       </main>
     </>
   );
