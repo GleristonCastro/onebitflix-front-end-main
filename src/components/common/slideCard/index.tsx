65813eb710be626c1e013066e5b4a736cfd1d3a4
Gleriston <xpanium@gmail.com>
2023-07-18 10:02:33 -0300

    fix link to course

--- src/components/common/slideCard/index.tsx
+++ src/components/common/slideCard/index.tsx
@@ -9,7 +9,7 @@ interface Props {
 const SlideCard = function ({ course }: Props) {
   return (
     <>
-      <Link href={`/courses/${course.id}`}>
+      <Link href={`/course/${course.id}`}>
         <div className={styles.slide}>
           <img
             src={`${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl}`}
