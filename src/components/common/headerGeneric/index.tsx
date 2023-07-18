019628c66f2340cafa602cefc75a77474c02b71e
Gleriston <xpanium@gmail.com>
2023-07-18 17:05:25 -0300

    include paraments

--- src/components/common/headerGeneric/index.tsx
+++ src/components/common/headerGeneric/index.tsx
@@ -1,23 +1,29 @@
-import { Button, Container } from "reactstrap";
-import styles from "./styles.module.scss";
-import Link from "next/link";
+import Link from 'next/link';
+import { Button, Container } from 'reactstrap';
+import styles from './styles.module.scss';
 
 interface props {
   logoUrl: string;
-	btnUrl: string;
+  btnUrl: string;
   btnContent: string;
 }
 
-const HeaderGeneric = function ({ btnContent, btnUrl, logoUrl }: props) {
+const HeaderGeneric = function ({ logoUrl, btnUrl, btnContent }: props) {
   return (
     <>
       <div className={styles.header}>
         <Container className={styles.headerContainer}>
           <Link href={logoUrl}>
-            <img src="/logoOnebitflix.svg" alt="logoRegister" className={styles.headerLogo}/>
+            <img
+              src='/logoOnebitflix.svg'
+              alt='logoRegister'
+              className={styles.headerLogo}
+            />
           </Link>
           <Link href={btnUrl}>
-            <Button outline color="light" className={styles.headerBtn}>{btnContent}</Button>
+            <Button outline color='light' className={styles.headerBtn}>
+              {btnContent}
+            </Button>
           </Link>
         </Container>
       </div>
