cccfd536b14cca9a304381c60c6f04a9aedacbed
Gleriston <xpanium@gmail.com>
2023-07-18 17:01:07 -0300

    Include Spinner and protection page with session storage

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,52 +1,66 @@
-import Head from "next/head";
-import styles from "../styles/profile.module.scss";
-import UserForm from "@/src/components/profile/user";
-import HeaderAuth from "@/src/components/common/headerAuth";
-import Footer from "@/src/components/common/footer";
-import { Button, Col, Container, Row } from "reactstrap";
-import { useState } from "react";
-import PasswordForm from "@/src/components/profile/password";
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Button, Col, Container, Row } from 'reactstrap';
+import Footer from '@/src/components/common/footer';
+import HeaderAuth from '@/src/components/common/headerAuth';
+import PageSpinner from '@/src/components/common/spinner';
+import PasswordForm from '@/src/components/profile/password';
+import UserForm from '@/src/components/profile/user';
+import styles from '../styles/profile.module.scss';
 
 const UserInfo = function () {
-  const [form, setForm] = useState("userForm");
+  const [form, setForm] = useState('userForm');
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!sessionStorage.getItem('onebitflix-token')) {
+      router.push('/login');
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  if (loading) {
+    return <PageSpinner />;
+  }
 
   return (
     <>
       <Head>
-        <title>Onebitflix - Meus Dados</title>
-        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <title>Onebitflix - Meus dados</title>
+        <link rel='shortcut icon' href='/favicon.svg' type='image/x-icon' />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.footer}>
+      <main>
+        <div className={styles.header}>
           <HeaderAuth />
         </div>
-        <Container className={styles.gridContainer}>
+        <Container className='py-5'>
           <p className={styles.title}>Minha Conta</p>
-          <Row className="pt-3 pb-5">
+          <Row className='pt-3 pb-5'>
             <Col md={4} className={styles.btnColumn}>
               <Button
-                outline
-                className={styles.renderFormBtn}
-                style={{ color: form === "userForm" ? "#ff0044" : "white" }}
+                className={styles.renderForm}
+                style={{ color: form === 'userForm' ? '#FF0044' : 'white' }}
                 onClick={() => {
-                  setForm("userForm");
+                  setForm('userForm');
                 }}
               >
                 DADOS PESSOAIS
               </Button>
               <Button
-                outline
-                className={styles.renderFormBtn}
-                style={{ color: form === "passwordForm" ? "#ff0044" : "white" }}
+                className={styles.renderForm}
+                style={{ color: form === 'passwordForm' ? '#FF0044' : 'white' }}
                 onClick={() => {
-                  setForm("passwordForm");
+                  setForm('passwordForm');
                 }}
               >
                 SENHA
               </Button>
             </Col>
             <Col md>
-            {form === "userForm" ? <UserForm /> : <PasswordForm />}
+              {form === 'userForm' ? <UserForm /> : <PasswordForm />}
             </Col>
           </Row>
         </Container>
